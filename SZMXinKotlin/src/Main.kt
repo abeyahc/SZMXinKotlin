108ccd7fc7df8d627b3920b43dec4dae24c37ad5
Cory Cowden <corycowden@Corys-MacBook-Air.local>
2026-03-10 20:27:31 -0700

    AST and Values

--- SZMXinKotlin/src/Main.kt
+++ SZMXinKotlin/src/Main.kt
@@ -1,14 +1,32 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-fun main() {
-    val name = "Kotlin"
-    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-    // to see how IntelliJ IDEA suggests fixing it.
-    println("Hello, " + name + "!")
+// ---------- SZMX in KOTLIN ----------
 
-    for (i in 1..5) {
-        //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-        // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        println("i = $i")
-    }
+
+// ---- AST ----
+// sealed class: only allows certain types of subclasses
+sealed class ExprC {
+    data class NumC(val n: Double) : ExprC() // use: ExprC.NumC
+    data class StringC(val s: String) : ExprC()
+    data class IdC(val name: String) : ExprC()
+    data class IfC(val test: ExprC, val thenBranch: ExprC, val elseBranch: ExprC) : ExprC()
+    data class FunC(val params: List<String>, val body: ExprC) : ExprC()
+    data class AppC(val function: ExprC, val args: List<ExprC>) : ExprC()
+}
+
+
+// ---- Values ----
+typealias Environment = List<Pair<String, Value>> // delete this line when writing interp (just for compiling)
+
+// sealed class: only allows certain types of subclasses
+sealed class Value {
+    data class NumV(val n: Double) : Value() // use: Value.NumV
+    data class BoolV(val b: Boolean) : Value()
+    data class StringV(val s: String) : Value()
+    data class ClosV(val params: List<String>, val body: ExprC, val env: Environment) : Value() // fill in Environment when writing interp
+    data class PrimopV(val name: String, val arity: Int, val op: (List<Value>) -> Value) : Value()
+}
+
+
+// ---- Tests ----
+fun main() {
+
 }
